505f98af92e80ef19b3b25549d0b92a6833f7230
RavjotSandhu <nand2072@gmail.com>
2021-07-15 12:42:17 +0530

    fetch data

--- src/Shop.js
+++ src/Shop.js
@@ -1,9 +1,26 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function Shop() {
+  useEffect(()=>{
+    fetchItems()
+  },[]);
+ 
+  const [item,setItem]=useState([]);
+  
+  const fetchItems = async()=>{
+  const data = await fetch('https://fakestoreapi.com/products');
+  
+  const items = await data.json();
+  console.log(items);
+  setItem(items);
+};
+
   return (
     <div>
-      <h1>Shop Page</h1>
+      {item.map(item =>(
+        <h1 key={item.id}>{item.title}</h1>
+      ))}
     </div>
   );
 }
